27ca43b5036ba20e14df860008317033cafdb902
Youssouf da Silva <youssoufdasilva@gmail.com>
2019-08-03 07:57:38 +0000

    First Studio Commit

--- web/client.js
+++
@@ -1,7 +0,0 @@
-const sanityClient = require("@sanity/client");
-
-module.exports = sanityClient({
-  projectId: "uyj1rgh2", // you can find this in sanity.json
-  dataset: "coldteethdb", // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
-});
